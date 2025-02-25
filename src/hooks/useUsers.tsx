90855c3f4e62283c96cf329ba7e73d5587e2de02
Aleksei Khokhlov <lexasby@gmail.com>
2025-02-25 15:59:24 +0100

    add userspage

---
+++ src/hooks/useUsers.tsx
@@ -0,0 +1,36 @@
+import { useQuery } from "@tanstack/react-query";
+import { instance } from "../api/config";
+
+export interface User {
+  id: string;
+  username: string;
+  role: string;
+}
+
+export interface UsersApiResponse {
+  data: {
+    data: User[];
+    meta: {
+      itemsPerPage: number;
+      totalItems: number;
+      currentPage: number;
+      totalPages: number;
+      sortBy: [string, string][];
+    };
+    links: {
+      current: string;
+    };
+  };
+}
+
+async function fetchUsers(): Promise<User[]> {
+  const res = await instance.get<UsersApiResponse>("/users");
+  return res.data.data.data;
+}
+
+export function useUsers() {
+  return useQuery<User[], Error>({
+    queryKey: ["users"],
+    queryFn: fetchUsers,
+  });
+}
